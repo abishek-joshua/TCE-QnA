553b09281e026c5decc2dd70405626d630aa9ec7
abishek-joshua <abishekjoshua2001@gmail.com>
2022-03-09 12:46:52 +0530

    Google Authentication created

--- QnA/urls.py
+++ QnA/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django import views
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from login.views import login_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('',login_page,name='login_page'),
 
 ]
